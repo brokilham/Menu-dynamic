22547e1fb2eb803ede0e5a59bd5144b4d039cf01
brokilham <plato.ilham@gmail.com>
2018-05-10 12:42:32 +0700

    tambah view distribusi kelas

--- resources/views/backend/webpages_distribusi/distribusi_kelas/modal-add.blade.php
+++ resources/views/backend/webpages_distribusi/distribusi_kelas/modal-add.blade.php
@@ -1,5 +1,5 @@
 <!-- /.modal -->
-<div class="modal fade modal-scroll" id="modal-add" tabindex="-1" data-replace="true">
+<div class="modal fade modal-scroll" id="modal-add-distribusi-kelas" tabindex="-1" data-replace="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -9,51 +9,32 @@
             <div class="modal-body">
                 <form id = "frm-add-mstr-guru" class="form-horizontal" role="form" method="POST">
                     <div class="form-body">
-                        <div class="form-group">
-                            <label class="col-md-2 control-label">  Id</label>
-                            <div class="col-md-10">
-                                <input type="text" class="form-control"  id="txt_id" name="txt_id" >
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-md-2 control-label">Nama</label>
-                            <div class="col-md-10">
-                                <input type="text" class="form-control"  id="txt_nama" name="txt_nama" >
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-md-2 control-label">Alamat</label>
-                            <div class="col-md-10">
-                                <input type="text" class="form-control"  id="txt_alamat" name="txt_alamat" >
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-md-2 control-label">No telp</label>
-                            <div class="col-md-10">
-                                <input type="text" class="form-control"  id="txt_no_telp" name="txt_no_telp" >
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-md-2 control-label">No Email</label>
-                            <div class="col-md-10">
-                                <input type="text" class="form-control"  id="txt_email" name="txt_email" >
-                            </div>
-                        </div>
-                        <div class="form-group" style="display:none">
-                            <label class="col-md-2 control-label">Status</label>
-                            <div class="col-md-10">
-                                <select class="form-control"  id = "txt_status" name="txt_status" >
-                                    <option value = "active">Active</option>
-                                    <option value = "non_Active">Non Active</option>
-                                </select>
-                            </div>
-                        </div>
+                            <div class="form-group">
+                                <label class="col-md-2 control-label">Nama Siswa</label>
+                                <div class="col-md-10">
+                                    <select class="form-control" id= "slc_nama_siswa" name ="slc_nama_siswa">                 
+                                    </select>
+                                    <!--
+                                        <input type="text" class="form-control"  id="txt_id" name="txt_id" >
+                                    -->
+                                </div>
+                            </div> 
+                            <div class="form-group">
+                                <label class="col-md-2 control-label">Kelas</label>
+                                <div class="col-md-10">
+                                    <select class="form-control" id= "slc_kelas" name ="slc_kelas">                 
+                                    </select>
+                                    <!--
+                                        <input type="text" class="form-control"  id="txt_id" name="txt_id" >
+                                    -->
+                                </div>
+                            </div> 
                     </div>
                 </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn dark btn-outline" data-dismiss="modal">Close</button>
-                <button type="button" class="btn green" id="btn-submit-mstr-guru" value=''>Save</button>  
+                <button type="button" class="btn green" id="btn-submit-distribusi-jabatan" value=''>Save</button>  
             </div>
         </div>
     </div>
