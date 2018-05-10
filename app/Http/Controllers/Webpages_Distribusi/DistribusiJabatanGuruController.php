0c2533dae078d12ee08fa46c82214c68ac4f2839
brokilham <plato.ilham@gmail.com>
2018-05-10 12:40:26 +0700

    tambah controller distribusi, pelanggaran, bimbingan

--- app/Http/Controllers/Webpages_Distribusi/DistribusiJabatanGuruController.php
+++ app/Http/Controllers/Webpages_Distribusi/DistribusiJabatanGuruController.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\mstr_jabatan;
 use App\mstr_guru;
+use App\t_distribusi_jabatan;
+use auth;
+use DataTables;
+use Exception;
 
 class DistribusiJabatanGuruController extends Controller
 {
@@ -24,49 +28,26 @@ class DistribusiJabatanGuruController extends Controller
 
 
     public function create(Request $request){
-       /* DB::beginTransaction();
-    
-       try
+        try
         {
-   
-            $mstr_siswa = new mstr_siswa;      
-            $mstr_siswa->id = $request->txt_sis_nis;
-            $mstr_siswa->id_walimurid = "7777";
-            $mstr_siswa->nama = $request->txt_sis_nama;
-            $mstr_siswa->alamat = $request->txt_sis_alamat;
-            $mstr_siswa->path_foto = $request->txt_sis_path_foto;
-            $mstr_siswa->hobi = $request->txt_sis_hobi;
-            $mstr_siswa->jenis_kelamin = $request->txt_sis_jenis_kelamin;
-            $mstr_siswa->email = $request->txt_sis_email;
-            $mstr_siswa->no_telp = $request->txt_sis_no_telp;
-            $mstr_siswa->status = "active";
-            $mstr_siswa->created_by =  Auth::user()->id;
-            $mstr_siswa->save();
-
-            $mstr_walimurid = new mstr_walimurid;  
-            $mstr_walimurid->id = "7777";
-            $mstr_walimurid->id_siswa = $request->txt_sis_nis;  
-            $mstr_walimurid->nama = $request->txt_wal_nama;  
-            $mstr_walimurid->alamat = $request->txt_sis_alamat;  
-            $mstr_walimurid->path_foto = "this dummy path foto";  
-            $mstr_walimurid->hub_keluarga = $request->txt_wal_family;  
-            $mstr_walimurid->pekerjaan = $request->txt_wal_pekerjaan;  
-            $mstr_walimurid->jenis_kelamin = $request->txt_wal_jenis_kelamin;  
-            $mstr_walimurid->email = $request->txt_Wal_email;  
-            $mstr_walimurid->no_telp = $request->txt_wal_no_telp;  
-            $mstr_walimurid->save();
-
-            $result = (($mstr_siswa == TRUE) && ($mstr_walimurid == TRUE))? "S":"F";
+           
+            $t_distribusi_jabatan = new t_distribusi_jabatan;
+            $t_distribusi_jabatan->id_guru = $request->slc_nama_guru;
+            $t_distribusi_jabatan->id_jabatan = $request->slc_jabatan;
+            $t_distribusi_jabatan->created_by =  Auth::user()->id;
+            $t_distribusi_jabatan->status = "active";  
+            $t_distribusi_jabatan->save();
+            
+            $result = ($t_distribusi_jabatan == TRUE)? "S":"F";
             $message = "-";
         }
         catch(Exception $e){
-            DB::rollback();
             $result = "E";
             $message = $e->getMessage();
         }
-
-        DB::commit();                 
-        return response()->json(['code' => $result, 'message' =>$message] );*/
+            
+       return response()->json(['code' => $result, 'message' =>$message] );
+       //return response()->json(['code' => $request->slc_nama_guru, 'message' =>$request->slc_jabatan] );
     }
 
     public function update(Request $request){
@@ -74,13 +55,23 @@ class DistribusiJabatanGuruController extends Controller
     }
 
     public function delete(Request $request){
-   
+        try{
+           
+            $return =   t_distribusi_jabatan::where('id', $request->id)->update(['status' => 'non_active']);
+            $result = ($return == 1)? "S":"F";
+            $message = "-";
+        }
+        catch(Exception $e){
+            $result = "E";
+            $message = $e->getMessage();
+        }
+  
+        return response()->json(['code' =>  $result, 'message' =>$message] );
     }
 
-    public function getall_mstr_siswa(){
+    public function getall_distribusi_jabatan(){
         
-        //$mstr_siswa = mstr_siswa::where('status', 'active');
-
-        //return DataTables::of($mstr_siswa)->make(true);
+        $t_distribusi_jabatan = t_distribusi_jabatan::where('status', 'active');
+        return DataTables::of($t_distribusi_jabatan)->make(true);
     }
 }
--- app/Http/Controllers/Webpages_Distribusi/DistribusiKelasSiswaController.php
+++ app/Http/Controllers/Webpages_Distribusi/DistribusiKelasSiswaController.php
@@ -1,10 +1,60 @@
 <?php
-
-namespace App\Http\Controllers;
-
+namespace App\Http\Controllers\Webpages_Distribusi;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use auth;
+use DataTables;
+use Exception;
 
 class DistribusiKelasSiswaController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('backend.webpages_distribusi.distribusi_kelas.main');
+    }
+
+    public function create(Request $request){
+        /*try
+        {
+           
+            $t_distribusi_jabatan = new t_distribusi_jabatan;
+            $t_distribusi_jabatan->id_guru = $request->slc_nama_guru;
+            $t_distribusi_jabatan->id_jabatan = $request->slc_jabatan;
+            $t_distribusi_jabatan->created_by =  Auth::user()->id;
+            $t_distribusi_jabatan->status = "active";  
+            $t_distribusi_jabatan->save();
+            
+            $result = ($t_distribusi_jabatan == TRUE)? "S":"F";
+            $message = "-";
+        }
+        catch(Exception $e){
+            $result = "E";
+            $message = $e->getMessage();
+        }
+            
+       return response()->json(['code' => $result, 'message' =>$message] );
+       //return response()->json(['code' => $request->slc_nama_guru, 'message' =>$request->slc_jabatan] );
+        */
+    }
+
+    public function update(Request $request){
+      
+    }
+
+    public function delete(Request $request){
+        
+        /*try{
+           
+            $return =   t_distribusi_jabatan::where('id', $request->id)->update(['status' => 'non_active']);
+            $result = ($return == 1)? "S":"F";
+            $message = "-";
+        }
+        catch(Exception $e){
+            $result = "E";
+            $message = $e->getMessage();
+        }
+  
+        return response()->json(['code' =>  $result, 'message' =>$message] );
+        */
+    }
 }
