130a9b6bf16fa9b2ea94c6f7d057394463b4f261
brokilham <plato.ilham@gmail.com>
2018-05-29 16:25:12 +0700

    tambah maste ket approval dan ket realisasi

---
+++ app/mstr_ket_approval.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class mstr_ket_approval extends Model
+{
+    //
+}
---
+++ app/mstr_ket_realisasi.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class mstr_ket_realisasi extends Model
+{
+    //
+}
